6ab4601e6a603f169bc3dbe61931c81642f9d657
jyotibista <zyotieebista12@gmail.com>
2026-03-06 08:36:22 +0545

    minors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import Portfolio from './components/Portfolio'
 
 export default function App() {
   return (
     <div>
-      <Portfolio/>
+      <Portfolio />
     </div>
   )
 }
